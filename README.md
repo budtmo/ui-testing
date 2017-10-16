746d17403e8649de63335b843a7b555ed097b1f0
budi utomo <budi.utomo@zalando.de>
2017-10-16 20:15:18 +0200

    Added sample UI Test project for msite by using appium

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Sample test projects written in java. It contains the following:
 
 1. [UI test for android using appium](android) - TODO
 2. [UI test for iOS using appium](iOS) - TODO
-3. [UI test for mobile website using appium](msite) - TODO
+3. [UI test for mobile website using appium](msite)
 4. [UI test for website using selenium](website)
---
+++ msite/src/test/java/SampleTest.java
@@ -0,0 +1,53 @@
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SampleTest {
+
+    private static final String URL = "http://127.0.0.1:4723/wd/hub";
+    private WebDriver driver;
+
+    @Before
+    public void prepare() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+        desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
+        driver = new AndroidDriver<WebElement>(new URL(URL), desiredCapabilities);
+    }
+
+    @Given("^google search page$")
+    public void googlePage() throws InterruptedException {
+        driver.get("https://www.google.de/");
+    }
+
+    @When("^user search for selenium")
+    public void searchForSelenium() {
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("selenium");
+        element.submit();
+    }
+
+    @Then("^user should see result page")
+    public void assertResultPage() {
+        driver.findElement(By.id("ires")).isDisplayed();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
