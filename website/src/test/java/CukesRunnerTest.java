80dc5a8c5bbe9509027f1b26e77063b55483ce81
budi utomo <budi.utomo@zalando.de>
2017-10-16 20:12:03 +0200

    Replaced JUnit with TestNG

--- website/src/test/java/CukesRunnerTest.java
+++ website/src/test/java/CukesRunnerTest.java
@@ -1,8 +1,7 @@
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
+
 @CucumberOptions(features = "src/test/resources/features", plugin = {"pretty", "html:target/cucumber-html-report"})
-public class CukesRunnerTest {
+public class CukesRunnerTest extends AbstractTestNGCucumberTests {
 }
