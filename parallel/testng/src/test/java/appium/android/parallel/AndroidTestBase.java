25cefa9cc32c7e805964fb769d857a7e9a4477c4
butomo1989 <budi.ut.1989@gmail.com>
2018-05-02 23:40:13 +0200

    Using the newest appium client

--- parallel/testng/src/test/java/appium/android/parallel/AndroidTestBase.java
+++ parallel/testng/src/test/java/appium/android/parallel/AndroidTestBase.java
@@ -1,31 +1,34 @@
 package appium.android.parallel;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Base64;
 
 public class AndroidTestBase {
     private static final String URL = "http://127.0.0.1:4444/wd/hub";
     private static final String APK = "/opt/sample_apk_debug.apk";
 
-    protected AppiumDriver<WebElement> driver;
+    protected AndroidDriver<WebElement> driver;
 
     @Parameters({"platformVersion", "deviceName", "browserName"})
     @BeforeClass
-    public void beforeClass(String platformVersion, String deviceName, String browserName) throws MalformedURLException {
+    public void beforeClass(@Optional("7.0") String platformVersion, @Optional("ce11160bbaee9c3b05") String deviceName,
+                            @Optional("android") String browserName) throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM, Platform.ANDROID);
         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, platformVersion);
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
@@ -36,6 +39,19 @@ public class AndroidTestBase {
         }
 
         driver = new AndroidDriver<>(new URL(URL), desiredCapabilities);
+    }
+
+    public static void decoder(String base64, String pathFile) {
+        try (FileOutputStream imageOutFile = new FileOutputStream(pathFile)) {
+            // Converting a Base64 String into Image byte array
+            byte[] ByteArray = Base64.getMimeDecoder().decode(base64);
+            System.out.println(ByteArray);
+            imageOutFile.write(ByteArray);
+        } catch (FileNotFoundException e) {
+            System.out.println("Image not found" + e);
+        } catch (IOException ioe) {
+            System.out.println("Exception while reading the Image " + ioe);
+        }
     }
 
     @AfterClass
--- parallel/testng/src/test/java/appium/android/parallel/testcases/AndroidAppTest.java
+++ parallel/testng/src/test/java/appium/android/parallel/testcases/AndroidAppTest.java
@@ -1,11 +1,27 @@
 package appium.android.parallel.testcases;
 
 import appium.android.parallel.AndroidTestBase;
+import io.appium.java_client.android.AndroidStartScreenRecordingOptions;
+import io.appium.java_client.screenrecording.ScreenRecordingUploadOptions;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class AndroidAppTest extends AndroidTestBase {
+
+    public static final String PATH_FILE = "/tmp/test.mp4";
+
+    @BeforeMethod
+    public void setUp(){
+        ScreenRecordingUploadOptions uOpt = new ScreenRecordingUploadOptions().withRemotePath(PATH_FILE);
+
+        AndroidStartScreenRecordingOptions rOpt = new AndroidStartScreenRecordingOptions().
+                withUploadOptions(uOpt);
+
+        driver.startRecordingScreen(rOpt);
+    }
 
     @Test
     public void searchForAppiumConf(){
@@ -17,5 +33,11 @@ public class AndroidAppTest extends AndroidTestBase {
 
         WebElement resultField = driver.findElementById("result");
         Assert.assertEquals(resultField.getText(), "10");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        String result = driver.stopRecordingScreen();
+        decoder(result, PATH_FILE);
     }
 }
