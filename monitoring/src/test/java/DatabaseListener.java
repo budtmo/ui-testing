87f84e699c6bb7b94cbccf1464ff477864d77d0d
butomo1989 <budi.ut.1989@gmail.com>
2019-04-10 21:44:28 +0700

    Cleaned up

--- monitoring/src/test/java/DatabaseListener.java
+++ monitoring/src/test/java/DatabaseListener.java
@@ -2,24 +2,30 @@ import org.influxdb.dto.Point;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.xml.XmlTest;
 
 import java.util.concurrent.TimeUnit;
 
 public class DatabaseListener implements ITestListener {
+    static XmlTest config;
+
     @Override
     public void onTestStart(ITestResult iTestResult) {
     }
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
+        sendTestResult(iTestResult, "PASSED");
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
+        sendTestResult(iTestResult, "FAILED");
     }
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
+        sendTestResult(iTestResult, "SKIPPED");
     }
 
     @Override
@@ -28,23 +34,24 @@ public class DatabaseListener implements ITestListener {
 
     @Override
     public void onStart(ITestContext iTestContext) {
+        config = iTestContext.getCurrentXmlTest();
     }
 
     @Override
     public void onFinish(ITestContext iTestContext) {
-        this.sendTestClassResult(iTestContext);
     }
 
-    public void sendTestClassResult(ITestContext iTestContext) {
-        Point point = Point.measurement("testclass")
+    public void sendTestResult(ITestResult iTestResult, String status) {
+        Point content = Point.measurement("result")
                 .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
-                .tag("name", iTestContext.getAllTestMethods()[0].getTestClass().getName())
-                .tag("device", iTestContext.getCurrentXmlTest().getParameter("deviceName"))
-                .tag("android version", iTestContext.getCurrentXmlTest().getParameter("platformVersion"))
-                .tag("number of passed tests", String.valueOf(iTestContext.getPassedTests().size()))
-                .tag("number of failed tests", String.valueOf(iTestContext.getFailedTests().size()))
-                .addField("duration", (iTestContext.getEndDate().getTime() - iTestContext.getStartDate().getTime()))
-                .build();
-        Database.save(point);
+                .tag("testclass", iTestResult.getTestClass().getName())
+                .tag("name", iTestResult.getName())
+                .tag("os", config.getParameter("browserName"))
+                .tag("device", config.getParameter("deviceName"))
+                .tag("android version", config.getParameter("platformVersion"))
+                .tag("status", status)
+                .addField("duration", (iTestResult.getEndMillis() - iTestResult.getStartMillis())).build();
+
+        Database.save(content);
     }
 }
