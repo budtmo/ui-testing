e76eaae7ff693e127cb29d45732d80122b4dd7ec
butomo1989 <budi.ut.1989@gmail.com>
2019-04-16 21:45:18 +0700

    Executed each scenario as a separate TestNG test

--- monitoring/src/test/java/CukesRunnerTest.java
+++ monitoring/src/test/java/CukesRunnerTest.java
@@ -1,22 +1,42 @@
 import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.PickleEventWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
-import java.io.IOException;
+@CucumberOptions(strict = true, plugin = "json:target/cucumber-report-feature-composite.json")
+public class CukesRunnerTest {
+    private TestNGCucumberRunner testNGCucumberRunner;
 
-@CucumberOptions(features = "src/test/resources/features", plugin = {"pretty", "html:target/cucumber-html-report"})
-public class CukesRunnerTest extends AbstractTestNGCucumberTests {
-    // For parallel test -> https://github.com/cucumber/cucumber-jvm/tree/master/testng
     public static String os, device, version;
 
     @Parameters({"browserName", "deviceName", "platformVersion"})
-    @BeforeClass
-    public void beforeClass(@Optional("android") String browserName, @Optional("ce11160bbaee9c3b05") String deviceName,
-                            @Optional("7.0") String platformVersion) throws IOException {
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass (@Optional("android") String browserName, @Optional("ce11160bbaee9c3b05") String deviceName,
+                            @Optional("7.0") String platformVersion) throws Exception {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
         os = browserName;
         device = deviceName;
         version = platformVersion;
+    }
+
+    @Test(groups = "cucumber", description = "Runs Cucumber Scenarios", dataProvider = "scenarios")
+    public void scenario(PickleEventWrapper pickleEvent, CucumberFeatureWrapper cucumberFeature) throws Throwable {
+        testNGCucumberRunner.runScenario(pickleEvent.getPickleEvent());
+    }
+
+    @DataProvider
+    public Object[][] scenarios() {
+        return testNGCucumberRunner.provideScenarios();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
     }
 }
--- monitoring/src/test/resources/features/calculator.feature
+++ monitoring/src/test/resources/features/calculator.feature
@@ -4,3 +4,8 @@ Feature: Addition operation
         Given calculator app
         When user sum up 3 and 7
         Then the result must be 10
+
+    Scenario: User want to sum up between 2 numbers [FAILED TEST]
+            Given calculator app
+            When user sum up 3 and 6
+            Then the result must be 10
